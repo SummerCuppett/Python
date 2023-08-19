d02287465959605bad9175c7e34b74d79b1202f0
Summer Cuppett <103083659+SummerCuppett@users.noreply.github.com>
2023-08-19 00:17:35 -0400

    Add files via upload

---
+++ 1.2quotes.py
@@ -0,0 +1,11 @@
+﻿# *_* coding： UTF-8 *_*
+# 开发团队：DATA SUMMER LLC
+# 网站：http://datasummer.net/
+#      http://cuppett.atwebpages.com/
+# 1.2 输出中英文版的“时间不等人”
+
+print("╔═════════════════════╗")
+print("║      时间不等人。                        ║")
+print("║      Time and tide wait for no man.      ║")
+print("╚═════════════════════╝")
+
